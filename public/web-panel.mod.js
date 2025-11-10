54951add649ad68aff16c02d59895e5fc7e2be58
Victor Velasquez <victorvelas.lst@gmail.com>
2025-11-09 20:08:03 -0500

    Pulir detalles del menu lateral

--- public/web-panel.mod.js
+++ public/web-panel.mod.js
@@ -2,10 +2,40 @@
 
 const WPM = (function () {
 
+    const $_updateOptions = function($combo, options) 
+    {
+        while ($combo.options.length > 0) {
+            $combo.remove(0);
+        }
+        console.log(options);
+        options.unshift({id: 0, text: 'Seleccione'});
+        options.forEach(function (opt) {
+            const newOption = document.createElement('option');
+            newOption.value = (opt.id) === 0 ? '' : opt.id.toString();
+            newOption.text = opt.text;
+            $combo.appendChild(newOption);
+        });
+        $combo.value = '';
+        $combo.dispatchEvent(new Event('change'));
+        console.log($combo);
+    }
+
 
     const _ = {
         setFormConfig: function (configB64) {
-            console.log(JSON.parse(atob(configB64)));
+            const conf = JSON.parse(atob(configB64));
+            if (parseInt(conf?.id || 0) > 0)
+            {
+                if (conf.targets.type !== null) {
+                    $_updateOptions(document.getElementById(`wpi-id${conf.targets.type}`), conf.targets.list);
+                    $_updateOptions(document.getElementById(`wpi-id${conf.targets.type === 'empresa' ? 'local' : 'empresa'}`), []);
+                } else {
+                    $_updateOptions(document.getElementById('wpi-idempresa'), []);
+                    $_updateOptions(document.getElementById('wpi-idlocal'), []);
+                }
+                return;
+            }
+
         }
     };
     return _;
--- src/views/main.php
+++ src/views/main.php
@@ -44,32 +44,44 @@
         </div>
         <div class="item-wpi full-height-wpi">
             <div class="full-height-wpi display-flex">
-                <div class="margin-auto">
-                    <form action="/" method="post">
+                <div class="margin-auto form-box-wpi">
+                    <form method="post" class="form-content-wpi">
                         <div class="wpi-form-container">
-                            <img id="wpi-img-logo" src="" alt="El logo va aqui" />
+                            <img id="wpi-img-logo" src="" alt="El logo va aqui" width="90%" />
                         </div>
                         <div class="wpi-form-container">
                             <h1 id="wpi-form-title">
                                 <span class="wpi-outlet">
-                                    Seleccione un servicio para continuar
+                                    Seleccione un servicio
                                 </span>
                             </h1>
                         </div>
                         <div class="wpi-form-container">
-                            <label for="wpi-idempresa">Empresa: </label>
-                            <select name="idempresa" id="wpi-idempresa">
-                                <option value="">Seleccione</option>
-                            </select>
+                            <div class="display-block-wpi">
+                                <label for="wpi-idempresa">Empresa: </label>
+                            </div>
+                            <div class="display-block-wpi">
+                                <select name="idempresa" id="wpi-idempresa" class="select-wpi">
+                                    <option value="">Seleccione</option>
+                                </select>
+                            </div>
                         </div>
                         <div class="wpi-form-container">
-                            <label for="wpi-idlocal">Local: </label>
-                            <select name="idlocal" id="wpi-idlocal">
-                                <option value="">Seleccione</option>
-                            </select>
+                            <div class="display-block-wpi">
+                                <label for="wpi-idlocal">Local: </label>
+                            </div>
+                            <div class="display-block-wpi">
+                                <select name="idlocal" id="wpi-idlocal" class="select-wpi">
+                                    <option value="">Seleccione</option>
+                                </select>
+                            </div>
                         </div>
                         <div class="wpi-form-container">
-
+                            <br>
+                            <label for="btn-submit-wpi" class="btn-wpi">
+                                Ingresar
+                            </label>
+                            <input type="submit" id="btn-submit-wpi" value="" style="display: none !important;">
                         </div>
                     </form>
                 </div>
